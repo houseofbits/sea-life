8b6e348e6359016ab53284874e2b7be56a25f252
Krists <kpudzens@gmail.com>
2021-11-17 07:06:31 +0200

    a

---
+++ app/js/Helpers/Constants.ts
@@ -0,0 +1,13 @@
+export enum PuzzleLayersEnum {
+    ORGANS_LAYER = 0,
+    SKIN_LAYER = 1
+}
+
+export enum PuzzleElementStateEnum {
+    NONE = 0,
+    DRAG = 1,
+    CAPTURED = 2,
+    PLACED = 3
+}
+
+export const PUZZLE_PIECE_CAPTURE_DISTANCE = 50;
